0794c7825c04c219c7a3355a515e67e5c8a7fd76
tildebetabot@gmail.com <tildebetabot@gmail.com>
2017-07-05 12:59:51 +0300

    Improvements

--- voice.py
+++ voice.py
@@ -5,18 +5,21 @@ import audioop
 import sys
 
 from pocketsphinx import Decoder
+from fuzzywuzzy import process
 import pyaudio
 
 
 class VoiceDetector:
+
     """
     A class for detecting speech
     """
     def __init__(self, *, chunk: int = 1024, rate: int = 16000,
                  channels: int = 1,
                  data_format=pyaudio.paInt16,
-                 model_dir='pocketsphinx/model',
-                 data_dir='pocketsphinx/test/data'):
+                 model_dir: str = 'pocketsphinx/model',
+                 data_dir: str = 'pocketsphinx/test/data',
+                 stream=None):
         self.data_format = data_format
         self.model_dir = model_dir
         self.data_dir = data_dir
@@ -32,34 +35,42 @@ class VoiceDetector:
         config.set_string('-logfn', location)
         self.decoder = Decoder(config)
 
-        p = pyaudio.PyAudio()
-        self.stream = p.open(frames_per_buffer=self.chunk,
-                             channels=self.channels,
-                             format=self.data_format,
-                             rate=self.rate,
-                             input=True)
+        # Configure a pyaudio stream or use the provided one
+        if stream is None:
+            p = pyaudio.PyAudio()
+            self.stream = p.open(frames_per_buffer=self.chunk,
+                                 channels=self.channels,
+                                 format=self.data_format,
+                                 rate=self.rate,
+                                 input=True)
+        else:
+            self.stream = stream
 
         self.threshold = self.get_threshold()
 
+        self.silence_buffer = collections.deque(maxlen=self.rate // self.chunk)
+        self.buffer = []
+
     def get_threshold(self, samples=50) -> int:
+        # threshold is the average of 1/5 of the loudest samples
         values = sorted([abs(audioop.avg(self.stream.read(self.chunk), 4)) ** 0.5 for _ in range(samples)], reverse=True)
-        print(values)
         valid = slice(None, int(samples * 0.2))
-        print(int(sum(values[valid])))
         threshold = max(700, int(sum(values[valid]) / (samples * 0.2)) + 100)
 
         print('Threshold set at: {}'.format(threshold))
         return threshold
 
-    def decode(self, data: list) -> Union[str, None]:
-        if not data:
+    def decode(self, buffer: list) -> Union[str, None]:
+        if not buffer:
             return
 
+        # try recognizing speech from buffer
         self.decoder.start_utt()
-        for entry in data:
+        for entry in buffer:
             self.decoder.process_raw(entry, False, False)
         self.decoder.end_utt()
 
+        # either the words recognized or None if no words were recognized
         hyp = self.decoder.hyp()
 
         if hyp is None:
@@ -67,21 +78,47 @@ class VoiceDetector:
         else:
             return hyp.hypstr
 
+    def poll(self, data=None):
+        data = data or self.stream.read(self.chunk)
+        self.silence_buffer.append(abs(audioop.avg(data, 4)) ** 0.5)
+
+        talking = any(val > self.threshold for val in self.silence_buffer)
+
+        if talking:
+            self.buffer.append(data)
+        else:
+            command = self.decode(self.buffer)
+            self.buffer.clear()
+
+            if command:
+                self.invoke(command)
+
     def run(self):
-        silence_buffer = collections.deque(maxlen=self.rate // self.chunk)
-        buffer = []
-
         while True:
-            data = self.stream.read(self.chunk)
-            silence_buffer.append(abs(audioop.avg(data, 4)) ** 0.5)
+            self.poll()
 
-            talking = any(val > self.threshold for val in silence_buffer)
+    def invoke(self, command: str):
+        commands = {
+            'play': self.play
+        }
+        command, *args = command.split()
+        closest_command, certainty = process.extractOne(command, commands.keys())
 
-            if talking:
-                buffer.append(data)
-            else:
-                print(self.decode(buffer))
-                buffer.clear()
+        if certainty < 90:
+            return
+
+        commands[closest_command](args)
+
+    def play(self, args: list):
+        if not path.isdir('songs'):
+            return
+
+        songs = path.listdir('songs')
+        name = ' '.join(args)
+
+        best_match = process.extractOne(name, songs)
+
+        print(best_match)
 
 
 if __name__ == '__main__':
